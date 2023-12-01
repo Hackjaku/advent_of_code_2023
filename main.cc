3241453978675563e7b58f51c1f05243fb63adab
Hackjaku <stressfest@rocketmail.com>
2023-12-01 11:57:50 +0100

    UPDATE

--- main.cc
+++ main.cc
@@ -1,3 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
 int main() {
-    return 0;
+    fstream data("../inputs/01.txt", ios::in);
+
+    string line;
+    while (getline(data, line)) {
+        cout << line << endl;
+    }
 }
